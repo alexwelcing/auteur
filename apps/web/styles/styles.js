9ffddf8c33a2b136e210eb693b26d1bdf26aba59
alexwelcing <alexwelcing@gmail.com>
2023-09-20 12:54:54 -0400

    saving before upgrades

---
+++ apps/web/styles/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const CompanyCard = styled.div`
+  background-color: #f1f1f1;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 20px;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+`;
+
+export const CompanyName = styled.h2`
+  font-size: 24px;
+  color: #333;
+`;
