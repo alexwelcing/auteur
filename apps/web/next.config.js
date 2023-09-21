87c0e579693a6f46158849ea027420fbd9ee78b3
alexwelcing <alexwelcing@gmail.com>
2023-09-20 21:23:33 -0400

    test on config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,49 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/steven-tey/novel",
-        permanent: true,
-      },
-      {
-        source: "/sdk",
-        destination: "https://www.npmjs.com/package/novel",
-        permanent: true,
-      },
-      {
-        source: "/npm",
-        destination: "https://www.npmjs.com/package/novel",
-        permanent: true,
-      },
-      {
-        source: "/svelte",
-        destination: "https://github.com/tglide/novel-svelte",
-        permanent: false,
-      },
-      {
-        source: "/vue",
-        destination: "https://github.com/naveennaidu/novel-vue",
-        permanent: false,
-      },
-      {
-        source: "/vscode",
-        destination:
-          "https://marketplace.visualstudio.com/items?itemName=bennykok.novel-vscode",
-        permanent: false,
-      },
-      {
-        source: "/feedback",
-        destination: "https://github.com/steven-tey/novel/issues",
-        permanent: true,
-      },
-      {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/novel",
-        permanent: true,
-      },
-    ];
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
   productionBrowserSourceMaps: true,
 };
